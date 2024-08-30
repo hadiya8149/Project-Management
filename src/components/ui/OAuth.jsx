b67a4a66f239ca17bd043ed8737c1d0692b4fcf3
hadiya8149 <hadiya8149@gmail.com>
2024-08-31 04:05:09 +0500

    google icon

--- src/components/ui/OAuth.jsx
+++ src/components/ui/OAuth.jsx
@@ -1,11 +1,11 @@
-import GoogleIcon from '@mui/icons-material/Google';
+import GoogleIcon from '../../assets/google_icon.png';
 export default function SignInWithGoogleButton(){
     const handleGoogleClick=()=>{
         console.log("google sigin clicked")
     }
     return(
         <button type="button" className="btn btn-light signin" onClick={handleGoogleClick}>
-           <GoogleIcon/>
+           <img style={{height:'18px', margin:'3px'}} src={GoogleIcon}></img>
             Continue with Google
         </button>
     );
