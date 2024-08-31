c7bda9feefcd54cf502d701c1d25cb7dd579d71f
hadiya8149 <hadiya8149@gmail.com>
2024-08-31 07:10:48 +0500

    login page css

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import SignInWithGoogleButton from '../../components/ui/OAuth.jsx';
-import planning_schedule_concept from '../../assets/flat-design-content-management-system-illustrated 1.png';
+// import planning_schedule_concept from '../../assets/company-employees-sharing-thoughts-ideas.png';
 import './auth.css';
 export default function Login() {
   return (
@@ -16,23 +16,23 @@ export default function Login() {
       <div>
         <div className='card card-login m-auto'>
 
-          <h1 className='card-title' style={{ fontSize: '30px' }}>Welcome to DeskUs</h1>
-          <div className='card-body'>
+          <h1 className='card-title login' style={{ fontSize: '30px' }}>Welcome to DeskUs</h1>
+          <div className='card-body login'>
             <p className='card-text' style={{ fontSize: '15px', fontWeight: '100' }}>Your one click Management App!</p>
             <form className='text-align-left'>
 
               <div className="mb-3 text-align-left">
-                <label htmlFor="exampleInputEmail1" id="emailLabel" className="form-label">Email</label>
-                <input type="email" placeholder='yourmail@example.com' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <label htmlFor="InputEmail" id="emailLabel" className="form-label">Email</label>
+                <input type="email" style={{ padding:'8px', background:'#eef5fd54'}} placeholder='yourmail@example.com' className="form-control" id="InputEmail" aria-describedby="emailHelp" />
               </div>
               <div className="mb-3 text-align-left">
-                <label htmlFor="exampleInputPassword1" id="passwordLabel" className="form-label">Password</label>
-                <input type="password" placeholder='........' className="form-control" id="exampleInputPassword1" />
+                <label htmlFor="InputPassword" id="passwordLabel" className="form-label">Password</label>
+                <input type="password"  style={{background:'#eef5fd54'}} className="form-control" id="InputPassword" />
               </div>
               <div className="mb-3 form-check">
                 <Link className='forgot-password-link' to='forgot_password'>Forgot Password?</Link>
               </div>
-              <button type="submit" className="btn btn-primary signin">Sign In</button>
+              <button type="submit" className="btn btn-light signin">Sign In</button>
 
               <p className='card-text' style={{ fontSize: '14px', margin: '8px', padding: '2px' }}>
                 Don't have an account <Link to='/signup' className='card-text signup-link'>signup?</Link>
@@ -48,8 +48,8 @@ export default function Login() {
 
       </div>
     </div>
-    <img style={{position:'absolute',bottom: '-56px',
-    left: '-89px'}} src={planning_schedule_concept}></img>
+    {/* <img style={{position:'absolute',bottom: '-56px',
+    left: '-89px', height:'600px'}} src={planning_schedule_concept}></img> */}
 
     </div>
   );
